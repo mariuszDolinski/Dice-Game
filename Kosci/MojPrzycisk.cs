2fc37f30c3033b452cd3253885c20e909192865b
Mariusz <Mariusz@MARIUSZ-PC>
2023-01-26 09:55:18 +0100

    Dodaj pliki projektów.

---
+++ Kosci/MojPrzycisk.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Kosci
+{
+    class MojPrzycisk : Button
+    {
+        public MojPrzycisk()
+        {
+            SetStyle(ControlStyles.Selectable, false);
+        }
+    }
+}
---
+++ Kosci/PozycjaPola.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Kosci
+{
+    class PozycjaPola
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+
+        public override bool Equals(object obj)
+        {
+            return (X == ((PozycjaPola)obj).X) && (Y == ((PozycjaPola)obj).Y);
+        }
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+    }
+}
